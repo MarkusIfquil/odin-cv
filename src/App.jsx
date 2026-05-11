a642bff710d0ef9ce3b69242f76ad35f19b3dbb1
Mark Nesovanovic <mark.nesovanovic@gmail.com>
2026-05-11 12:00:31 +0200

    add input

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,99 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+    return (
+        <>
+            <Input />
+            <Output />
+        </>
+    );
 }
 
-export default App
+function Input() {
+    return (
+        <>
+            <DetailsSection />
+            <ListSection title={"Education"} listItems={[{ title: "AAA" }]} />
+            <ListSection
+                title={"Work experience"}
+                listItems={[{ title: "AAA" }]}
+            />
+        </>
+    );
+}
+
+function DetailsSection() {
+    return (
+        <>
+            <h1>Personal Details</h1>
+            <DetailInput legend={"Full name"} value={"Name"} />
+            <DetailInput legend={"Email"} value={"Email"} />
+            <DetailInput legend={"Phone number"} value={"Number"} />
+            <DetailInput legend={"Address"} value={"Somewhere"} />
+        </>
+    );
+}
+
+function ListSection({ title, listItems }) {
+    return (
+        <div>
+            <h1>{title}</h1>
+            {listItems.map((l) => (
+                <ListItem title={l.title} />
+            ))}
+        </div>
+    );
+}
+
+function ListItem({ title }) {
+    return (
+        <div>
+            <h1>{title}</h1>
+            <button type="">-</button>
+        </div>
+    );
+}
+
+function DetailInput({ legend, value }) {
+    return (
+        <>
+            <legend>{legend}</legend>
+            <input type="" name="" value={value} />
+        </>
+    );
+}
+
+function Output({ title, email, tel, address, sections }) {
+    return (
+        <>
+            <header>
+                <h1>{title}</h1>
+                <div class="contact">
+                    <Contact picture={""} text={email} />
+                    <Contact picture={""} text={tel} />
+                    <Contact picture={""} text={address} />
+                </div>
+                {sections}
+            </header>
+        </>
+    );
+}
+
+function Contact() {}
+
+function ListInput() {}
+
+function ListOutputItem() {}
+
+function ListOutput({ headerText, sections }) {
+    return (
+        <>
+            <header>{headerText}</header>
+            {sections.map((s) => (
+                <ListOutputItem section={s} />
+            ))}
+        </>
+    );
+}
+
+export default App;
