2d386bfb62551839ab7102cf9ec0a59e6dc53fd5
Mark Nesovanovic <mark.nesovanovic@gmail.com>
2026-05-11 13:22:03 +0200

    feat: add cv object state

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,69 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+    let [cv, setCv] = useState({
+        personal: {
+            name: "Madeleine",
+            email: "madeleine@fakeemail.com",
+            tel: "12 3456 7890",
+            locale: "Hell, Michigan",
+        },
+        education: [
+            {
+                start: "09/2022",
+                end: "07/2026",
+                locale: "Nowhere, Oklahoma",
+                place: "Clown College",
+                position: "Bachelors in clownery",
+            },
+        ],
+        professional: [
+            {
+                start: "09/2022",
+                end: "07/2026",
+                locale: "Nowhere, Oklahoma",
+                place: "Freelance clown",
+                position: "Clown",
+            },
+        ],
+    });
     return (
         <>
-            <Input />
-            <Output />
+            <Input cv={cv} setCv={setCv} />
+            <Output cv={cv} setCv={setCv} />
         </>
     );
 }
 
-function Input() {
+function Input({ cv, setCv }) {
     return (
         <>
-            <DetailsSection />
-            <ListSection
-                title={"Education"}
-                listItems={[{ title: "Random School" }]}
-            />
-            <ListSection
-                title={"Work experience"}
-                listItems={[{ title: "Random work" }]}
-            />
+            <DetailsSection personal={cv.personal} />
+            <ListSection title={"Education"} items={cv.education} />
+            <ListSection title={"Work experience"} items={cv.professional} />
         </>
     );
 }
 
-function DetailsSection() {
+function DetailsSection({ personal }) {
     return (
         <>
             <h1>Personal Details</h1>
-            <DetailInput legend={"Full name"} value={"Name"} />
-            <DetailInput legend={"Email"} value={"Email"} />
-            <DetailInput legend={"Phone number"} value={"Number"} />
-            <DetailInput legend={"Address"} value={"Somewhere"} />
+            <DetailInput legend={"Full name"} value={personal.name} />
+            <DetailInput legend={"Email"} value={personal.email} />
+            <DetailInput legend={"Phone number"} value={personal.tel} />
+            <DetailInput legend={"Address"} value={personal.locale} />
         </>
     );
 }
 
-function ListSection({ title, listItems }) {
+function ListSection({ title, items }) {
     return (
         <div>
             <h1>{title}</h1>
-            {listItems.map((l) => (
-                <ListItem title={l.title} />
+            {items.map((l) => (
+                <ListItem title={l.place} />
             ))}
         </div>
     );
@@ -66,20 +87,22 @@ function DetailInput({ legend, value }) {
     );
 }
 
-function Output({ name, email, tel, address, sections }) {
+function Output({ cv, setCv }) {
     return (
         <>
             <header>
-                <h1>{name}</h1>
+                <h1>{cv.personal.name}</h1>
                 <div class="contact">
-                    <Contact picture={""} text={email} />
-                    <Contact picture={""} text={tel} />
-                    <Contact picture={""} text={address} />
+                    <Contact picture={""} text={cv.personal.email} />
+                    <Contact picture={""} text={cv.personal.tel} />
+                    <Contact picture={""} text={cv.personal.locale} />
                 </div>
-                {sections.map((s) => (
-                    <ListOutput section={s} />
-                ))}
             </header>
+            <ListOutput header="Education" section={cv.education} />
+            <ListOutput
+                header="Professional Experience"
+                section={cv.professional}
+            />
         </>
     );
 }
@@ -93,29 +116,28 @@ function Contact({ picture, text }) {
     );
 }
 
-function ListOutput(section) {
+function ListOutput({ header, section }) {
     return (
         <>
-            <header>{section.header}</header>
-            {section.items.map((s) => (
-                <ListOutputItem section={s} />
+            <header>{header}</header>
+            {section.map((s) => (
+                <ListOutputItem item={s} />
             ))}
         </>
     );
 }
 
-function ListOutputItem(item) {
+function ListOutputItem({ item }) {
     return (
         <>
             <div class="time-and-place">
                 <p>
-                    {item.fromTime}-{item.toTime}
+                    {item.start}-{item.end}
                 </p>
-                <p>{item.place}</p>
+                <p>{item.locale}</p>
             </div>
-            <h1>{item.loc}</h1>
+            <h1>{item.place}</h1>
             <p>{item.position}</p>
-            <p>{item.description}</p>
         </>
     );
 }
